d49688785d4c3e21a1089c5398ae224a0b04f777
Aleksandr <aleksandrinomel@mail.ru>
2019-11-03 11:16:51 +0300

    HW lesson_7

---
+++ lesson_7/task_2.py
@@ -0,0 +1,41 @@
+# Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
+# заданный случайными числами на промежутке [0; 50). Выведите на экран исходный и отсортированный массивы.
+import random
+
+
+def merge_sort(alist):
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        left_half = alist[:mid]
+        right_half = alist[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                alist[k] = left_half[i]
+                i = i + 1
+            else:
+                alist[k] = right_half[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(left_half):
+            alist[k] = left_half[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(right_half):
+            alist[k] = right_half[j]
+            j = j + 1
+            k = k + 1
+
+
+alist = [random.uniform(0, 50) for _ in range(10)]
+print(alist)
+merge_sort(alist)
+print(alist)
